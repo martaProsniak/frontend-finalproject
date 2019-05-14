73373f49c9334d735c69577e2283f95aeae8c4f2
Marta <martycjas13@gmail.com>
2019-05-14 19:44:09 +0200

    user-edit component in progress

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {UserDetailsComponent} from '../components/user-details/user-details.component';
 import {UsersListComponent} from '../components/users-list-admin/users-list.component';
+import {UserEditComponent} from '../components/user-edit/user-edit.component';
 
 
 const appRoutes: Routes = [
   { path: 'users/:id', component: UserDetailsComponent, pathMatch: 'full' },
-  { path: 'users', component: UsersListComponent, pathMatch: 'full' }
+  { path: 'users', component: UsersListComponent, pathMatch: 'full' },
+  { path: 'users/edit/:id', component: UserEditComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {UsersService} from '../../services/users.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+
+  constructor(private usersService: UsersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.usersService
+      .getUserDetails(id)
+      .subscribe(result => {
+        this.user = result;
+      });
   }
 
 }
