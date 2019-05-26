0e88ecaa43d4f1fbb90c7cce30f83238a6d61640
Marta <martycjas13@gmail.com>
2019-05-26 22:35:31 +0200

    order class

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -47,5 +47,6 @@ export class CartComponent implements OnInit {
   removeFromCart(productId: number) {
     this.cartService.remove(this.cart.id, productId, this.cart)
       .subscribe(cart => this.cart = cart);
+    this.ngOnInit();
   }
 }
